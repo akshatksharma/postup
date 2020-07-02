3b69a9bdfff6d7c575bc9d0ae00326cbc719d5ee
Akshat Sharma <akshatksharma@ip-172-31-41-9.us-east-2.compute.internal>
2020-07-02 02:33:02 -0500

    added signup feature + connected to database

---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+
+session_id("user");
+session_start();
+require "database.php";
+
+$username =  $_POST["username"];
+$passwordRaw = $_POST["password"];
+
+$passwordSH = password_hash($passwordRaw, PASSWORD_DEFAULT);
+
+$stmt = $mysqli->prepare("insert into users (username, password) values (?,?)");
+if (!$stmt) {
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
+
+$stmt->bind_param('ss', $username, $passwordSH);
+$stmt->execute();
+$stmt->close();
+
+header('Location: home.php');
+exit;
