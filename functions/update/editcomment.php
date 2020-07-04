920c8353930bdaa226382a39b8670c81ce182614
Akshat Sharma <akshatksharma@ip-172-31-41-9.us-east-2.compute.internal>
2020-07-03 20:13:56 -0500

    Restructured and organized directories

---
+++ functions/update/editcomment.php
@@ -0,0 +1,26 @@
+<?php
+session_id("user");
+session_start();
+require "../../database.php";
+
+$postid = $_SESSION['currentPostID'];
+$commentid = (int) $_GET["id"];
+$comment = (string) $_POST["commenttext"];
+
+if (!hash_equals($_SESSION['token'], $_POST['token'])) {
+    die("Request forgery detected");
+}
+
+
+$stmt = $mysqli->prepare("update comments set comment=? where id=?");
+if (!$stmt) {
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
+
+$stmt->bind_param('si', $comment, $commentid);
+$stmt->execute();
+$stmt->close();
+
+header('Location: ../../post.php?id=' . $postid);
+exit;
